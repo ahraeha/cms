23caee7a16bd2cceaed3dcfa6577caae0a43f55b
jigi <ahraeha@naver.com>
2023-03-01 16:39:01 +0900

    feat : [006-CUSTOMER-BALANCE] 고객에 대한 잔액 조회 구현 잔액 조회 잔액 변경

--- src/main/java/com/zerobase/cms/controller/CustomerController.java
+++ src/main/java/com/zerobase/cms/controller/CustomerController.java
@@ -1,15 +1,19 @@
 package com.zerobase.cms.controller;
 
+import com.zerobase.cms.domain.customer.ChangeBalanceForm;
 import com.zerobase.cms.domain.customer.CustomerDto;
 import com.zerobase.cms.domain.model.Customer;
 import com.zerobase.cms.exception.CustomException;
 import com.zerobase.cms.exception.ErrorCode;
-import com.zerobase.cms.service.CustomerService;
+import com.zerobase.cms.service.customer.CustomerBalanceService;
+import com.zerobase.cms.service.customer.CustomerService;
 import com.zerobase.config.JwtAuthenticationProvider;
 import com.zerobase.domain.common.UserVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +25,7 @@ public class CustomerController {
 
   private final JwtAuthenticationProvider provider;
   private final CustomerService customerService;
+  private final CustomerBalanceService customerBalanceService;
 
   @GetMapping("/getInfo")
   public ResponseEntity<CustomerDto> getInfo(@RequestHeader(name = "X-AUTH-TOKEN") String token) {
@@ -31,4 +36,11 @@ public class CustomerController {
     return ResponseEntity.ok(CustomerDto.from(c));
   }
 
+  @PostMapping("/balance")
+  public ResponseEntity<Integer> changeBalance(@RequestHeader(name = "X-AUTH-TOKEN") String token, @RequestBody
+      ChangeBalanceForm form) {
+    UserVo vo = provider.getUserVo(token);
+
+    return ResponseEntity.ok(customerBalanceService.changeBalance(vo.getId(), form).getCurrentMoney());
+  }
 }
