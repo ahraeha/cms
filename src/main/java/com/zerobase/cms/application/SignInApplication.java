0a76bec601860ead18f596b83370f0046e06121a
jigi <ahraeha@naver.com>
2023-03-01 16:22:03 +0900

    feat : [005-SELLER-SIGN-UP-IN] 셀러에 대한 사인업, 사인인 구현 /seller/* 에 대한 token filter 적용

--- src/main/java/com/zerobase/cms/application/SignInApplication.java
+++ src/main/java/com/zerobase/cms/application/SignInApplication.java
@@ -2,9 +2,11 @@ package com.zerobase.cms.application;
 
 import com.zerobase.cms.domain.SignInForm;
 import com.zerobase.cms.domain.model.Customer;
+import com.zerobase.cms.domain.model.Seller;
 import com.zerobase.cms.exception.CustomException;
 import com.zerobase.cms.exception.ErrorCode;
-import com.zerobase.cms.service.CustomerService;
+import com.zerobase.cms.service.customer.CustomerService;
+import com.zerobase.cms.service.seller.SellerService;
 import com.zerobase.config.JwtAuthenticationProvider;
 import com.zerobase.domain.common.UserType;
 import lombok.RequiredArgsConstructor;
@@ -15,14 +17,22 @@ import org.springframework.stereotype.Service;
 public class SignInApplication {
 
   private final CustomerService customerService;
+  private final SellerService sellerService;
   private final JwtAuthenticationProvider provider;
 
   public String customerLoginToken(SignInForm form) {
     //1.로그인 가능 여부
     Customer c = customerService.findValidCustomer(form.getEmail(),
         form.getPassword()).orElseThrow(() -> new CustomException(ErrorCode.LOGIN_CHECK_FAIL));
-    //2. 토큰을 발행
-    //3. 토른을 response한다.
+
     return provider.createToken(c.getEmail(), c.getId(), UserType.CUSTOMER);
+  }
+
+  public String sellerLoginToken(SignInForm form) {
+    //1.로그인 가능 여부
+    Seller c = sellerService.findValidSeller(form.getEmail(),
+        form.getPassword()).orElseThrow(() -> new CustomException(ErrorCode.LOGIN_CHECK_FAIL));
+
+    return provider.createToken(c.getEmail(), c.getId(), UserType.SELLER);
   }
 }
--- src/main/java/com/zerobase/cms/application/SignUpApplication.java
+++ src/main/java/com/zerobase/cms/application/SignUpApplication.java
@@ -4,9 +4,11 @@ import com.zerobase.cms.client.MailgunClient;
 import com.zerobase.cms.client.mailgun.SendMailForm;
 import com.zerobase.cms.domain.SignUpForm;
 import com.zerobase.cms.domain.model.Customer;
+import com.zerobase.cms.domain.model.Seller;
 import com.zerobase.cms.exception.CustomException;
 import com.zerobase.cms.exception.ErrorCode;
-import com.zerobase.cms.service.SignUpCustomerService;
+import com.zerobase.cms.service.customer.SignUpCustomerService;
+import com.zerobase.cms.service.seller.SellerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -19,13 +21,14 @@ public class SignUpApplication {
 
   private final MailgunClient mailgunClient;
   private final SignUpCustomerService signUpCustomerService;
+  private final SellerService sellerService;
 
   public void customerVerify(String email, String code) {
     signUpCustomerService.verifyEmail(email, code);
   }
 
   public String customerSignUp(SignUpForm form) {
-    if (signUpCustomerService.isEmailExist(form.getEmail())){
+    if (signUpCustomerService.isEmailExist(form.getEmail())) {
       //exception
       throw new CustomException(ErrorCode.ALREADY_REGISTER_USER);
     } else {
@@ -37,11 +40,10 @@ public class SignUpApplication {
           .from("tester@ahnmytester.com")
           .to(form.getEmail())
           .subject("Verification Email")
-          .text(getVerificationEmailBody(c.getEmail(), c.getName(), code))
+          .text(getVerificationEmailBody(c.getEmail(), c.getName(), "customer", code))
           .build();
 
       log.info("Send email result : " + mailgunClient.sendEmail(sendMailForm).getBody());
-      mailgunClient.sendEmail(sendMailForm);
 
       signUpCustomerService.changeCustomerValidateEmail(c.getId(), code);
       return "회원 가입에 성공하였습니다.";
@@ -49,14 +51,42 @@ public class SignUpApplication {
 
   }
 
+  public void sellerVerify(String email, String code) {
+    sellerService.verifyEmail(email, code);
+  }
+
+  public String sellerSignUp(SignUpForm form) {
+    if (sellerService.isEmailExist(form.getEmail())) {
+      //exception
+      throw new CustomException(ErrorCode.ALREADY_REGISTER_USER);
+    } else {
+      Seller s = sellerService.signUp(form);
+
+      String code = getRandomCode();
+
+      SendMailForm sendMailForm = SendMailForm.builder()
+          .from("tester@ahnmytester.com")
+          .to(form.getEmail())
+          .subject("Verification Email")
+          .text(getVerificationEmailBody(s.getEmail(), s.getName(), "seller", code))
+          .build();
+
+      log.info("Send email result : " + mailgunClient.sendEmail(sendMailForm).getBody());
+
+      sellerService.changeSellerValidateEmail(s.getId(), code);
+      return "회원 가입에 성공하였습니다.";
+    }
+  }
+
   private String getRandomCode() {
     return RandomStringUtils.random(10, true, true);
   }
-  private String getVerificationEmailBody(String email, String name, String code) {
+
+  private String getVerificationEmailBody(String email, String name, String type, String code) {
 
     StringBuilder builder = new StringBuilder();
     return builder.append("hello").append(name).append("! Please Click Link for Verification.\n\n")
-        .append("http://localhost:8080/signup/verify/customer?email=")
+        .append("http://localhost:8080/signup/" + type + "/verify/?email=")
         .append(email)
         .append("&code=")
         .append(code).toString();
