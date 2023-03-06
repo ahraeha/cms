df634de3212b82cc077a245fa07a6fd4fb4472fa
jigi <ahraeha@naver.com>
2023-03-06 20:34:32 +0900

    feat : [014-SHOW-CART] 카트 보기 - 가격 /수량 /아이템 변경에 따른 대응

--- order-api/src/main/java/com/zerobase/cms/order/domain/redis/Cart.java
+++ order-api/src/main/java/com/zerobase/cms/order/domain/redis/Cart.java
@@ -14,6 +14,7 @@ import org.springframework.data.redis.core.RedisHash;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @RedisHash("cart")
 public class Cart {
 
@@ -21,6 +22,10 @@ public class Cart {
   private Long customerId;
   private List<Product> products = new ArrayList<>();
   private List<String> messages = new ArrayList<>();
+
+  public Cart(Long customerId) {
+    this.customerId = customerId;
+  }
 
   public void addMessage(String message) {
     messages.add(message);
@@ -56,6 +61,7 @@ public class Cart {
   @NoArgsConstructor
   @AllArgsConstructor
   public static class ProductItem {
+
     private Long id;
     private String name;
     private Integer count;
